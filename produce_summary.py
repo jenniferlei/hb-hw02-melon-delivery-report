3f3a2871155c7154aad7c92267c0b96a6f2f3307
Jennifer Lei <jenniferlei1928@gmail.com>
2021-10-15 21:19:30 +0000

    Change program to one function and call 3 times

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,30 @@
+def sales_reports(the_file):
+    '''prints sales data in a readable format from the sales report'''
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+# For day 1, open sales report and assign to the_file1 variable
+# Call sales report function with the_file1 as parameter
 print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+the_file1 = open("um-deliveries-20140519.txt")
+sales_reports(the_file1)
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
+# For day 2, open sales report and assign to the_file2 variable
+# Call sales report function with the_file2 as parameter
 print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+the_file2 = open("um-deliveries-20140520.txt")
+sales_reports(the_file2)
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
+# For day 3, open sales report and assign to the_file3 variable
+# Call sales report function with the_file3 as parameter
 print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+the_file3 = open("um-deliveries-20140521.txt")
+sales_reports(the_file3)
