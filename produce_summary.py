acc091a59592f4e6c1f79fb353704ac20bb5cd22
Jennifer Lei <jenniferlei1928@gmail.com>
2021-10-15 23:12:23 +0000

    shorten code

--- produce_summary.py
+++ produce_summary.py
@@ -1,30 +1,21 @@
-def sales_reports(the_file):
-    '''prints sales data in a readable format from the sales report'''
+def melon_delivery(day, path):
+    '''given day and file path for the day's deliveries, produce report in readable format'''
+    # print the day
+    print(f"Day {day}")
+    the_file = open(path)
+
+    # for each line in the file, strip right side white space and split data by |
     for line in the_file:
         line = line.rstrip()
         words = line.split('|')
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        # assign variables to each word
+        melon, count, amount = words
 
         print(f"Delivered {count} {melon}s for total of ${amount}")
     the_file.close()
 
-# For day 1, open sales report and assign to the_file1 variable
-# Call sales report function with the_file1 as parameter
-print("Day 1")
-the_file1 = open("um-deliveries-20140519.txt")
-sales_reports(the_file1)
-
-# For day 2, open sales report and assign to the_file2 variable
-# Call sales report function with the_file2 as parameter
-print("Day 2")
-the_file2 = open("um-deliveries-20140520.txt")
-sales_reports(the_file2)
-
-# For day 3, open sales report and assign to the_file3 variable
-# Call sales report function with the_file3 as parameter
-print("Day 3")
-the_file3 = open("um-deliveries-20140521.txt")
-sales_reports(the_file3)
+# Call melon delivery function with day number and delivery report path
+melon_delivery(1, "um-deliveries-20140519.txt")
+melon_delivery(2, "um-deliveries-20140520.txt")
+melon_delivery(3, "um-deliveries-20140521.txt")
